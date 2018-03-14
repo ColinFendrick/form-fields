d632551ea54754dd09a6c03506e8f8cb285b1e3c
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-14 09:31:21 -0400

    Did some basic stuff with forms

--- src/field.js
+++ src/field.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 const Field = ({ name, size, type }) => {
 	return (
-		<input placeholder={placeholderText} />
+		<input placeholder={name} type={type}/>
 	);
 };
 
--- src/fields.js
+++ src/fields.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Field from './field';
+import myData from './placeholder-data.json';
 
 class Forms extends Component {
 	render() {
+		const fieldList = myData.map(({ name, type }) => {
+			return <Field name={name} type={type} />;
+		});
 		return (
 			<div>
 				<form>
-					<Field placeholder="text stuff whaa" />
+					{fieldList}
 					<button type="submit">add</button>
 				</form>
 			</div>
