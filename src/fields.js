ce2108249af220f20ba184ffdbe2c4f830af9c92
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-14 08:57:09 -0400

    Some basic stylings and whatever

---
+++ src/fields.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Forms extends Component {
+	render() {
+		return (
+			<div>
+				<form>
+					<input placeholder="placeholdertext">
+					</input>
+					<button type="submit">add</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default Forms;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Fields from './fields';
+
+ReactDOM.render( 
+	<div>
+		<Fields />
+	</div>,
+	document.querySelector('#app')
+);
